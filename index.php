b0a2e2e275eb5d1ad79433b8a28e46b09028725d
pedrodese <pedrohenriquedasilvapereira12@gmail.com>
2023-09-11 17:25:25 -0300

    Tipos de dados em PHP

--- index.php
+++ index.php
@@ -8,5 +8,9 @@ $booleanTrue = true;
 $array = ['Pedro', 'Rafaela', 'Luiz'];
 
 var_dump($array);
+var_dump($booleanFalse);
+var_dump($booleanTrue);
+var_dump($numFloat);
+var_dump($numInt);
     
 ?>
